7f2b0757474c486825dbd27d7ca6e53c3d7dda04
zach <mrztryan@gmail.com>
2022-01-14 13:41:48 -0500

    angular init, migrating content

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  title: string = "Kapri";
+
+  constructor(
+    private titleService: Title,
+    private metaService: Meta
+  ) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Furry Artist, Commissions, Art, NSFW Furry Art'},
+      {name: 'description', content: 'Commission info and links for furry artist Kapri.'},
+      {name: 'robots', content: 'index, follow'},
+      {name: 'image', content: 'img/img_banner.png'}
+    ]);
+  }
+
+}
