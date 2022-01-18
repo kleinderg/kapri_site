a60b37fb2d2bd25a1bb003eb822d203340010d4c
zach <mrztryan@gmail.com>
2022-01-18 17:01:13 -0500

    *whispers* haaaa push it

--- TODO.md
+++ TODO.md
@@ -4,5 +4,6 @@
 - [ ] Commission info state for home page
   - Picture hides, show info
   - Commission Info --> Commission Info (Hide)
-- [ ] Form field restyle to match the pixelated look
-- [ ] Checkout API integration
+- [x] Form field restyle to match the pixelated look
+- [x] Checkout API integration
+- [ ] Remove the black bg corners on text fields
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,21 @@ import { CommissionsComponent } from './pages/commissions/commissions.component'
 import { PayKapriComponent } from './pay-kapri/pay-kapri.component';
 import { SuccessComponent } from './pay-kapri/success/success.component';
 import { FailComponent } from './pay-kapri/fail/fail.component';
-import { PaymentHelperService } from './pay-kapri/service/payment-helper.service';
+import { PaymentHelperService } from './pay-kapri/service/payment-helper.service';import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlJt3zyyo3bd55oiAEi_j90HWvdDbAG0Y",
+  authDomain: "kaprihorn.firebaseapp.com",
+  projectId: "kaprihorn",
+  storageBucket: "kaprihorn.appspot.com",
+  messagingSenderId: "1066698926832",
+  appId: "1:1066698926832:web:764e92e64d8bdffcd5b035",
+  measurementId: "G-S0TH8HDLE4"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
